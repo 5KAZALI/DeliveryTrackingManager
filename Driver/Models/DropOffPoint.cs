036b1a6fff34ebf18954d3126c9edd76a983fce0
5KAZALI <fkaragoz@asu.edu>
2020-05-28 14:53:26 -0700

    New Modal is added
    DropOffPoints for Driver

--- Driver/Models/DropOffPoint.cs
+++ Driver/Models/DropOffPoint.cs
@@ -7,5 +7,14 @@ namespace Driver.Models
 {
     public class DropOffPoint
     {
+        public int Id { get; set; }
+        public string ReceiverName { get; set; }
+        public string Street { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+
+        //KuryeId: Teslimati Gerceklestirenin Id
+        public string UserId { get; set; }
     }
 }
